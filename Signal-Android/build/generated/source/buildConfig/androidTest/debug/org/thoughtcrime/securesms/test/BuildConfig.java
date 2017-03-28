ff3c7c4680c9def2afe7737c5edf0a17e0273cc3
amatanat <a_metanet@hotmail.com>
2017-03-28 11:14:00 +0200

    Signal-android build some part

---
+++ Signal-Android/build/generated/source/buildConfig/androidTest/debug/org/thoughtcrime/securesms/test/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.thoughtcrime.securesms.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.thoughtcrime.securesms.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+  // Fields from default config.
+  public static final long BUILD_TIMESTAMP = 1476360217375L;
+  public static final boolean DEV_BUILD = false;
+  public static final String REDPHONE_MASTER_URL = "https://redphone-master.whispersystems.org";
+  public static final String REDPHONE_PREFIX_NAME = ".whispersystems.org";
+  public static final String REDPHONE_RELAY_HOST = "relay.whispersystems.org";
+  public static final String TEXTSECURE_URL = "https://192.168.178.57:8080";
+  public static final String USER_AGENT = "OWA";
+}
---
+++ Signal-Android/build/generated/source/buildConfig/debug/org/thoughtcrime/securesms/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.thoughtcrime.securesms;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.thoughtcrime.securesms";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 186;
+  public static final String VERSION_NAME = "3.13.0";
+  // Fields from default config.
+  public static final long BUILD_TIMESTAMP = 1476360297391L;
+  public static final boolean DEV_BUILD = false;
+  public static final String REDPHONE_MASTER_URL = "https://redphone-master.whispersystems.org";
+  public static final String REDPHONE_PREFIX_NAME = ".whispersystems.org";
+  public static final String REDPHONE_RELAY_HOST = "relay.whispersystems.org";
+  public static final String TEXTSECURE_URL = "https://192.168.178.57:8080";
+  public static final String USER_AGENT = "OWA";
+}
